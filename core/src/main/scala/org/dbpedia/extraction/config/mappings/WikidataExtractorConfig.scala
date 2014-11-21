85617b2981417cd2c16d1f380eaf16aafc8fa67d
unknown <ismayilov@7440-C4W2WZ1.unccd.int>
2014-11-21 15:40:05 +0100

    trying

--- core/src/main/scala/org/dbpedia/extraction/config/mappings/WikidataExtractorConfig.scala
+++ core/src/main/scala/org/dbpedia/extraction/config/mappings/WikidataExtractorConfig.scala
@@ -4,25 +4,28 @@ package org.dbpedia.extraction.config.mappings
 //Test class of client
 object WikidataExtractorConfig{
 
-  val test1= new WdtkMapping(propertyTransformations =  List(new ReplaceFunction("dbo:birthPlace")), objectTransformations = List.empty)
+  val replaceFunction =new ReplaceFunction("dbo:birthPlace")
+  val test1= new WdtkMapping(propertyTransformations = List(replaceFunction), objectTransformations = List.empty)
+  test1.executeAll()
  // val test2 =
 
   def mapping(value:String="") = Map(
-    "P19" -> test1.executeAll()
+    "P19" -> test1.propertyArray(0)
   //  "P434" -> new WdtkMapping(propertyTransformations = List(new ReplaceFunction(("owl:sameAs"))), objectTransformations = List(new AddPrefixFunction("http://musicbrainz.org/artist/", value)))
   )
 }
 
 //The Command Interface
 trait TRansformationFunction {
-  def execute()
+  def execute():String
 }
 
 //The Invoker Class
 class WdtkMapping(val propertyTransformations:List[TRansformationFunction],val objectTransformations: List[TRansformationFunction]) {
+  val propertyArray=Array[String]()
   def executeAll(): Unit = {
     for (propertyTrans <- propertyTransformations) {
-      propertyTrans.execute()
+      propertyArray:+propertyTrans.execute()
     }
 
     for (objTrans <- objectTransformations) {
@@ -40,13 +43,13 @@ class Functions {
 //Concrete command class
 class ReplaceFunction(newValue:String="") extends TRansformationFunction {
   def replace():String = newValue
-  def execute() = replace()
+  def execute():String = replace()
 }
 
 //Concrete command class
 class AddPrefixFunction (prefix:String="", value:String="") extends TRansformationFunction {
   def addPrefix():String = prefix + value
-  def execute() = addPrefix()
+  def execute():String = addPrefix()
 }
 
 
--- core/src/main/scala/org/dbpedia/extraction/mappings/wikidata/WikidataR2RExtrcactor.scala
+++ core/src/main/scala/org/dbpedia/extraction/mappings/wikidata/WikidataR2RExtrcactor.scala
@@ -1,58 +1,50 @@
 package org.dbpedia.extraction.mappings
 
-import com.sun.xml.internal.ws.addressing.policy.AddressingPrefixMapper
-import org.dbpedia.extraction.config.mappings.{WdtkMapping, AddPrefix, WikidataExtractorConfig}
-import org.dbpedia.extraction.mappings.{PageContext, JsonNodeExtractor}
-import org.dbpedia.extraction.ontology.{OntologyProperty, Ontology}
+import org.dbpedia.extraction.config.mappings.{ReplaceFunction, WikidataExtractorConfig}
+import org.dbpedia.extraction.destinations.{Dataset, Quad}
+import org.dbpedia.extraction.mappings.{JsonNodeExtractor, PageContext}
+import org.dbpedia.extraction.ontology.Ontology
 import org.dbpedia.extraction.util.Language
-import org.dbpedia.extraction.destinations.{Dataset, Quad, DBpediaDatasets}
-import org.dbpedia.extraction.wikiparser.{JsonNode}
+import org.dbpedia.extraction.wikiparser.JsonNode
 import org.wikidata.wdtk.datamodel.interfaces._
-import collection.mutable.ArrayBuffer
-import scala.language.reflectiveCalls
-import scala.collection.JavaConversions._
 
+import scala.collection.JavaConversions._
 import scala.collection.mutable.ArrayBuffer
+import scala.language.reflectiveCalls
 
 /**
  * Created by ali on 10/26/14.
  */
 class WikidataR2RExtrcactor(
-                                    context : {
+                               context : {
                                       def ontology : Ontology
                                       def language : Language
                                     }
-                                    )
+                            )
   extends JsonNodeExtractor
 {
 
   // this is where we will store the output
-  val WikidataTestDataSet = new Dataset("wikidata-geo")
+  val WikidataTestDataSet = new Dataset("wikidata-r2r")
   override val datasets = Set(WikidataTestDataSet)
 
-
-  val simpleTranformProperties=List("P434", "P435", "P436", "P982", "P966", "P1004", "P686", "P661", "P227","P244",
-    "P269", "P349","P646", "P638", "P662", "P685","P214")
 
   override def extract(page : JsonNode, subjectUri : String, pageContext : PageContext): Seq[Quad] =
   {
     // This array will hold all the triples we will extract
     val quads = new ArrayBuffer[Quad]()
-
     for ((statementGroup) <- page.wikiDataItem.getStatementGroups) {
       val claim = statementGroup.getStatements().get(0).getClaim()
       val property = claim.getMainSnak().getPropertyId().toString().replace("(PropertyId)", "")
       val propID=property.replace("http://data.dbpedia.org/resource/","")
-
       claim.getMainSnak() match {
         case mainSnak: ValueSnak => {
           mainSnak.getValue() match {
             case value: ItemIdValue => {
-              if (simpleTranformProperties.contains(propID)){
-                val p = WikidataExtractorConfig.mapping(propID)
-                val o = new WdtkMapping(propertyTransformations = List.empty, objectTransformations = List(new AddPrefix(WikidataExtractorConfig.prefixMapping(propID), value.toString)))
+                 val p = WikidataExtractorConfig.mapping().get("P19")
+                 println("test")
+//                val o =
                 //quads += new Quad(context.language, WikidataTestDataSet, subjectUri, p, o, page.wikiPage.sourceUri, null)
-              }
 
             }
             case _ =>
