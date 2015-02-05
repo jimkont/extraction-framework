db8c3b017d5ab78983a13e484a50cf80e8b63d5e
alismayilov <alismayilov@gmail.com>
2015-02-05 11:37:43 +0100

    wikidataReificationExtractor is ready

--- core/src/main/scala/org/dbpedia/extraction/mappings/wikidata/WikidataReificationExtractor.scala
+++ core/src/main/scala/org/dbpedia/extraction/mappings/wikidata/WikidataReificationExtractor.scala
@@ -22,10 +22,10 @@ class WikidataReificationExtractor(
   extends JsonNodeExtractor {
 
   private val rdfType = context.ontology.properties("rdf:type")
-//  private val rdfStatement = context.ontology.properties("rdf:Statement")
-//  private val rdfSubject = context.ontology.properties("rdf:subject")
-//  private val rdfPredicate= context.ontology.properties("rdf:predicate")
-//  private val rdfObject = context.ontology.properties("rdf:object")
+  private val rdfStatement = "http://www.w3.org/1999/02/22-rdf-syntax-ns#Statement"
+  private val rdfSubject = "http://www.w3.org/1999/02/22-rdf-syntax-ns#subject"
+  private val rdfPredicate = "http://www.w3.org/1999/02/22-rdf-syntax-ns#predicate"
+  private val rdfObject = "http://www.w3.org/1999/02/22-rdf-syntax-ns#object"
 
   // this is where we will store the output
   val WikidataReificationDataSet = new Dataset("wikidata-reification")
@@ -40,49 +40,80 @@ class WikidataReificationExtractor(
         statement => {
           val claim = statement.getClaim()
           val property = WikidataUtil.replacePropertyId(claim.getMainSnak().getPropertyId().toString)
-            claim.getQualifiers.foreach {
-              qualifier => {
-                claim.getMainSnak() match {
-                  case mainSnak: ValueSnak => {
-                    val value = mainSnak.getValue
-                    val statementUri= getStatementUri(subjectUri, property, getItemId(value))
-                    println(statementUri)
-                    val qualifierMap = splitQualifier(qualifier.toString)
-                    val qualifierProperty = qualifierMap.keys.head
-                    val qualifierValue = qualifierMap.get(qualifierProperty)
-//                    quads += new Quad(context.language, WikidataReificationDataSet, statementUri, rdfType, rdfStatement.uri, page.wikiPage.sourceUri)
-//                    quads += new Quad(context.language, WikidataReificationDataSet, statementUri, rdfSubject, subjectUri, page.wikiPage.sourceUri,null)
-//                    quads += new Quad(context.language, WikidataReificationDataSet, statementUri, rdfPredicate, property, page.wikiPage.sourceUri,null)
-//                    quads += new Quad(context.language, WikidataReificationDataSet, statementUri, rdfObject, value.toString, page.wikiPage.sourceUri,null)
-//                    println(subjectUri + " " + property + " " + value + " " )
-                  }
-                  case _ =>
-                }
+          claim.getQualifiers.foreach {
+            qualifier => {
+              claim.getMainSnak() match {
+                case mainSnak: ValueSnak => {
+                  val value = mainSnak.getValue
+                  val statementUri = WikidataUtil.getStatementUri(subjectUri, property, value)
+                  val qualifierMap = WikidataUtil.splitQualifier(qualifier.toString)
+                  val qualifierProperty = qualifierMap.keys.head
+                  val qualifierValue = qualifierMap.get(qualifierProperty)
+                  quads += new Quad(context.language, WikidataReificationDataSet, statementUri, rdfType, rdfStatement, page.wikiPage.sourceUri)
+                  quads += new Quad(context.language, WikidataReificationDataSet, statementUri, rdfSubject, subjectUri, page.wikiPage.sourceUri, null)
+                  quads += new Quad(context.language, WikidataReificationDataSet, statementUri, rdfPredicate, property, page.wikiPage.sourceUri, null)
+                  val datatype= getDatatype(value)
+                  quads += new Quad(context.language, WikidataReificationDataSet, statementUri, rdfObject, getValue(value), page.wikiPage.sourceUri, datatype)
+                  qualifierValue match {
+                    case Some(v) => quads += new Quad(context.language, WikidataReificationDataSet,
+                        statementUri, qualifierProperty, v, page.wikiPage.sourceUri, context.ontology.datatypes("xsd:string"))
 
+                  }
+                }
+                case _ =>
               }
+
             }
+          }
         }
       }
     }
     quads
   }
 
-  def getItemId(value:Value) = value match {
-      case v:ItemIdValue => WikidataUtil.replaceItemId(v.toString)
-      case _ => getHashId()
+  def getDatatype(value: Value) = value match {
+    case value: ItemIdValue => {
+      null
+    }
+    case value: StringValue => {
+      context.ontology.datatypes("xsd:string")
+    }
+
+    case value: TimeValue => {
+      context.ontology.datatypes("xsd:date")
+    }
+    case value: GlobeCoordinatesValue => {
+      context.ontology.datatypes("xsd:string")
+    }
+
+    case value: QuantityValue => {
+      context.ontology.datatypes("xsd:float")
+    }
   }
 
-  def getStatementUri(subject:String, property:String,itemId:String):String = {
-    subject+"_"+ property.replace(WikidataUtil.wikidataDBpNamespace, "").trim+"_" + itemId.replace(WikidataUtil.wikidataDBpNamespace,"")
-  }
+  def getValue(value: Value) = value match {
+    case value: ItemIdValue => {
+      val v = WikidataUtil.replaceItemId(value.toString)
+      v
+    }
+    case value: StringValue => {
+      val v = WikidataUtil.replaceString(value.toString)
+      v
+    }
 
-  def splitQualifier(qualifier:String):Map[String,String] = {
-    Map(qualifier.toString.split("::")(0) ->  qualifier.toString.split("::")(1))
-  }
+    case value: TimeValue => {
+      val v = value.getYear + "-" + value.getMonth + "-" + value.getDay
+      v
+    }
+    case value: GlobeCoordinatesValue => {
+      val v = value.getLatitude + " " + value.getLongitude
+      v
+    }
 
-  def getHashId():String={
-   "testString"
+    case value: QuantityValue => {
+      val v = value.getNumericValue.toString
+      v
+    }
   }
-
 }
 
--- core/src/main/scala/org/dbpedia/extraction/util/WikidataUtil.scala
+++ core/src/main/scala/org/dbpedia/extraction/util/WikidataUtil.scala
@@ -1,5 +1,9 @@
 package org.dbpedia.extraction.util
 
+import java.security.MessageDigest
+
+import org.wikidata.wdtk.datamodel.interfaces.{ItemIdValue, Value}
+
 /**
  * Created by ali on 2/1/15.
  */
@@ -18,6 +22,25 @@ object WikidataUtil {
 
   def replaceString(str:String):String = {
     str.replace("(String)","").trim()
+  }
+
+  def getItemId(value:Value) = value match {
+    case v:ItemIdValue => replaceItemId(v.toString).replace(wikidataDBpNamespace,"")
+    case _ => getHashId(value)
+  }
+
+  def getStatementUri(subject:String, property:String,value:Value):String = {
+    subject+"_"+ property.replace(WikidataUtil.wikidataDBpNamespace, "").trim+"_" + getItemId(value)
+  }
+
+  def splitQualifier(qualifier:String):Map[String,String] = {
+    Map(qualifier.toString.split("::")(0).trim ->  qualifier.toString.split("::")(1).trim)
+  }
+
+  //Hash function take string, make SHA hash of it.
+  def getHashId(value:Value):String={
+    val hash_string = value.toString
+    MessageDigest.getInstance("SHA").digest(hash_string.getBytes).toString
   }
 
   val wikidataDBpNamespace = Language("wikidata").resourceUri.namespace
